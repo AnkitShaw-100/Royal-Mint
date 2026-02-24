9c5ab2b381f0e51a86542c80dacc9f3d2f681d6a
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-02-24 10:10:00 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log("Attempting to connect to:", process.env.MONGODB_URI)
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Database connection error:");
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
