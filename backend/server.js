6cf8b920bef4a318d2f88f604f8f1b130fe4b7ca
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-03-05 23:21:34 +0530

    deployed links added

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://royal-mint-ankit.vercel.app"
+    ],
     credentials: true,
   })
 );
